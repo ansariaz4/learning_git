53c69c7cf47615b0dc6a4a2c35bc7c684a8d11ba
Azharuddin <78867109+ansariaz4@users.noreply.github.com>
2024-03-26 11:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 My name is Azhar
 and i am a devOps Engineer
 I have 4+ years of experienced  
-I anm looking for the job changes, but call floe  is very less
+I am looking for the job changes, but call flow  is very less
+But recently i joined into the company called switch media  
 
